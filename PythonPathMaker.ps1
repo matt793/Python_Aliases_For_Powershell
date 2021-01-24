02a3d9301b18ede2306b7055216b2bc618bb29b8
Matthew Lopez <61769072+matt793@users.noreply.github.com>
2021-01-24 16:42:14 -0700

    Global call for aliases when typing $profile

---
+++ PythonPathMaker.ps1
@@ -0,0 +1,9 @@
+﻿# First add 'Python_Aliases_For_Powershell.ps1' to the followin path:
+# C:\Users\$comp\Documents\WindowsPowerShell\Scripts\Python_Aliases_For_Powershell.ps1 ($comp stands for computer name, put in your name there)
+
+
+$comp = Read-Host "Input computer name here"
+$profile = C:\Users\$comp\Documents\WindowsPowerShell\Scripts\Python_Aliases_For_Powershell.ps1
+Set-ExecutionPolicy remotesigned
+
+# After running this script you can just type "$profile", and it will call in the aliases for useability.
