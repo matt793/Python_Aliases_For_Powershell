e48dcbb5197f1f9250cb690c30f9cb692dde08b5
Matthew Lopez <61769072+matt793@users.noreply.github.com>
2021-01-23 21:45:55 -0700

    Main powershell script.

---
+++ Python_Aliases_For_Powershell.ps1
@@ -0,0 +1,18 @@
+﻿# This powershell script adds Aliases to powershell, that make commands closer to python/linux.
+
+Set-Alias -Name print -Value Write-Host  
+
+
+Set-Alias -Name Fore -Value ForegroundColor
+
+
+Set-Alias -Name Back -Value BackgroundColor
+
+
+Set-Alias -Name prints -Value Write-Output
+
+
+Set-Alias -Name input -Value Read-Host
+
+
+Set-Alias -Name touch -Value New-Item
